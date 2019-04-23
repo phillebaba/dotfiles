4989b0b262c5b9dfd1469811870c4dc845ff9100
Philip Laine <philip.laine@gmail.com>
2019-04-23 19:48:04 +0000

    Change general bar layout
    - Add numbers to i3 workspace tabs
    - Add support for different network device names
    - Clean up unused variables

--- files/.config/polybar/launch.sh
+++ files/.config/polybar/launch.sh
@@ -3,5 +3,5 @@
 killall -q polybar
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$m polybar --reload top &
+  WIRELESS=$(ls /sys/class/net | grep wl) ETHERNET=$(ls /sys/class/net | grep enp) MONITOR=$m polybar --reload top &
 done
